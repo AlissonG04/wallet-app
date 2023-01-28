eb10e6524b4901f9804505afb175032644df6b26
Alisson Gabriel <eng.alissongabriel@gmail.com>
2023-01-28 13:08:28 -0300

    feat: validação de usuario

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const onClickLogin = () => {
+const validateUser = async (email) => {
+  try {
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
+    );
+    const user = await result.json();
+    return user;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const onClickLogin = async () => {
   const email = document.getElementById("input-email").value;
-  //const name = document.getElementById("input-name").value;
   if (email.length < 5 || !email.includes("@")) {
-    alert("Email Invalido.");
+    alert("Email invalido.");
     return;
   }
-
-  localStorage.setItem("@WalletApp:userEmail", email);
-  //localStorage.setItem("@WalletApp:userName", name);
+  const result = await validateUser(email);
+  if (result.error) {
+    alert("Falha ao validar e-mail");
+    return;
+  }
+  localStorage.setItem("@WalletApp:userEmail", result.email);
+  localStorage.setItem("@WalletApp:userName", result.name);
+  localStorage.setItem("@WalletApp:userId", result.id);
   window.open("./src/pages/home/index.html", "_self");
 };
